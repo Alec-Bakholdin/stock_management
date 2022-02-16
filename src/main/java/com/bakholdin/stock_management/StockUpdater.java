e321ba22daf1a2d01e3080b0815239df3c4268fe
Alec Bakholdin <alecbakholdin@gmail.com>
2022-02-15 21:44:25 -0500

    Put it all together into StockUpdater

--- src/main/java/com/bakholdin/stock_management/StockUpdater.java
+++ src/main/java/com/bakholdin/stock_management/StockUpdater.java
@@ -14,24 +14,25 @@ import java.util.Collection;
 @Log4j2
 @Component
 @RequiredArgsConstructor
-public class ScheduledTest {
+public class StockUpdater {
     private final StockRatingDelegate<ZacksRow> zacksStockRatingDelegate;
     private final StockRatingDelegate<TipRanksRow> tipRanksStockRatingDelegate;
     private final StockRatingDelegate<YahooRow> yahooRowStockRatingDelegate;
 
     @Scheduled(initialDelay = 1000, fixedRate = 1000000)
-    public void testZacks() {
+    public void UpdateStocks() {
         log.info("Fetching Zacks Data...");
         Collection<ZacksRow> rows = zacksStockRatingDelegate.fetchRows();
         log.info(String.format("Saving %d Zacks entries", rows.size()));
         zacksStockRatingDelegate.saveRows(rows);
-        //log.info("Fetching TipRanks Data...");
-        //Collection<TipRanksRow> rows2 = tipRanksStockRatingDelegate.fetchRows();
-        //for(var row : rows2) {
-        //    log.info(row);
-        //}
-        //log.info("Saving TipRanks data");
-        //tipRanksStockRatingDelegate.saveRows(rows2);
+        log.info("Saved all Zacks entries");
+
+        log.info("Fetching TipRanks Data...");
+        Collection<TipRanksRow> rows2 = tipRanksStockRatingDelegate.fetchRows();
+        log.info("Saving TipRanks data");
+        tipRanksStockRatingDelegate.saveRows(rows2);
+        log.info("Saved TipRanks Data");
+
         log.info("Fetching Yahoo Data...");
         Collection<YahooRow> yahooRows = yahooRowStockRatingDelegate.fetchRows();
         log.info("Saving Yahoo Data...");
